004261f729229cd127a8813e948f81d92bacfd33
Alex Licata <alerandom99@gmail.com>
2020-03-18 13:20:12 +0100

    test 2

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var app = require('express')();
 var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+var io = require('socket.io')(http, { origins: 'badlice.github.io:* http://badlice.github.io:* http://badlice.github.io:*' });
 const shortid = require('shortid');
 const { uuid } = require('uuidv4');
 
